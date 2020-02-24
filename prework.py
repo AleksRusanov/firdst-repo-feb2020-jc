553a35a66b84f1bed2b322e81b2dd0f2093bdbe2
CodingTemple <a9105842642@gmail.com>
2020-02-24 09:11:04 -0800

    First commit of Feb 2020 Cohort Alex

---
+++ prework.py
@@ -0,0 +1,57 @@
+def display_name(name):
+    print(name)
+
+display_name('Joel')
+
+# Prework Question 2- 100 odd numbers
+
+def print_odd():
+    for num in range(0,100):
+        if num % 2 != 0:
+            print(num)
+
+print_odd()
+
+
+#question3
+def max_num_in_list(a_list)
+    max = a_list[0]
+    for a in a list:
+        if a > max:
+            max = a
+            return max
+
+
+#quest3a
+def max_num (a_list):
+    return max(a_list)
+
+
+# 4 leap year
+
+def is_leap(year):
+    if year % 4 ==0 and (year % 400 ==0 or year % 100 != 0):
+        print ('true')
+    else
+        print ('false')
+
+
+
+def is_conscecutive(a_list):
+    total = 2
+    while total > 2:
+        test + a_list.pop(0)
+        if test == a_list[0]-1:
+            total = len(a_list)
+        else:
+            return False
+    return: True
+
+def iconsec(a_list):
+    for i in range(1, len (a_list)):
+        if a_list[i]-1!= a_list[i-1]:
+            return False
+    return True
+          
+
+
